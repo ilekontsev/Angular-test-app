d4f629cee543e79a79d6d9bf984360ebdf2ec392
Ivan <ivan.lek123@gmail.com>
2022-09-06 00:59:35 +0300

    [PF-2] stylezed chat

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
-const MODULES = [MatTabsModule, MatButtonModule];
+const MODULES = [
+  MatTabsModule,
+  MatButtonModule,
+  MatIconModule,
+  ScrollingModule,
+];
 
 @NgModule({
   imports: MODULES,
